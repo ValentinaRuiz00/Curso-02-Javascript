fb04534d015387276d1ca386d8e9bee591b5450d
ValentinaRuiz00 <valentinaruiztapias1234@gmail.com>
2024-07-02 20:56:55 -0500

    Explicación de interpolacion

---
+++ app.js
@@ -0,0 +1,11 @@
+//Interpolación
+nombreUsuario = "Valentina";
+let estado = false;
+
+//toUpperCase Para volver el string en mayuscula
+console.log(`Bienvenido: ${nombreUsuario.toUpperCase()}`);
+
+console.log(
+`${estado ? "en linea" : "ofline"}
+`
+);
