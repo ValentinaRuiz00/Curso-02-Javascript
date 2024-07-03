a0c699e0308df5f91e6ed10894aa79dedf552170
ValentinaRuiz00 <valentinaruiztapias1234@gmail.com>
2024-07-03 17:26:30 -0500

    Explicaion de los arrays, tipos de array y practica de un carrito de compras

--- app.js
+++ app.js
@@ -11,17 +11,54 @@
 // );
 
 //var,let,const
-let estado = true;
+// let estado = true;
 
-//la variable estado dentro del if fuciona como una variable independiente, pero unicamente en el if/scope
-if(estado){
-    console.log("Entró al if")
-    let estado=false;
-    console.log(estado);
+// //la variable estado dentro del if fuciona como una variable independiente, pero unicamente en el if/scope
+// if(estado){
+//     console.log("Entró al if")
+//     let estado=false;
+//     console.log(estado);
+// }
+
+// console.log(estado);
+// //const es una variable constante, no puede ser reemplazada
+
+
+// //array con push,pop,shift,unshift 
+//push =  añade uno o más elementos al final de un array y devuelve la nueva longitud del array.
+//unshift = agrega uno o más elementos al inicio del array, y devuelve la nueva longitud del array.
+//pop = elimina el último elemento de un array y lo devuelve. Este método cambia la longitud del array.
+//shift = elimina el primer elemento del array y lo retorna. Este método modifica la longitud del array.
+// const frutas = ["Sandía","Pera"];
+
+// frutas.push("Manzana");
+// frutas.unshift("Banano");
+// frutas.pop();
+// frutas.shift();
+
+// console.log(frutas);
+
+
+//Practica de carrito de compras
+function carrito() {
+    let frutas = [];
+    let continuar = true;
+
+    while (continuar) {
+        let fruta = prompt("🍓 ¿Qué fruta desea comprar? 🛒");
+        if (fruta) {
+            frutas.push(fruta);
+        }
+        continuar = confirm("🍓¿Desea agregar otra fruta?🛒");
+    }
+
+    return frutas;
 }
 
-console.log(estado);
-//const es una variable constante, no puede ser reemplazada
+let miCarrito = carrito();
+console.log("Frutas en el carrito:", miCarrito);
+
+  
 
 
 
