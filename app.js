20c7802b3388507fa9e419d11440046988c3ade9
ValentinaRuiz00 <valentinaruiztapias1234@gmail.com>
2024-07-03 10:54:38 -0500

    Explicacion de var,let y const

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
-//Interpolación
-nombreUsuario = "Valentina";
-let estado = false;
+// //Interpolación
+// nombreUsuario = "Valentina";
+// let estado = false;
 
-//toUpperCase Para volver el string en mayuscula
-console.log(`Bienvenido: ${nombreUsuario.toUpperCase()}`);
+// //toUpperCase Para volver el string en mayuscula
+// console.log(`Bienvenido: ${nombreUsuario.toUpperCase()}`);
 
-console.log(
-`${estado ? "en linea" : "ofline"}
-`
-);
+// console.log(
+// `${estado ? "en linea" : "ofline"}
+// `
+// );
+
+//var,let,const
+let estado = true;
+
+//la variable estado dentro del if fuciona como una variable independiente, pero unicamente en el if/scope
+if(estado){
+    console.log("Entró al if")
+    let estado=false;
+    console.log(estado);
+}
+
+console.log(estado);
+//const es una variable constante, no puede ser reemplazada
+
+
+
+
+    
