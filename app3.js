636eca0f8a25c99adab10ae4371112af8d30d799
ValentinaRuiz00 <valentinaruiztapias1234@gmail.com>
2024-07-09 19:14:43 -0500

    Practica #01 con DOM

--- app3.js
+++ app3.js
@@ -1,11 +1,26 @@
-console.log(document);
-console.log(document.getElementById("titulo-web"));
-console.log(document.getElementById("titulo-web").textContent);
-console.log(document.querySelector("#titulo-web"))
-console.log(document.querySelectorAll(".text-danger"))
-console.log(document.querySelectorAll(".container .text-danger"))
+// console.log(document);
+// console.log(document.getElementById("titulo-web"));
+// console.log(document.getElementById("titulo-web").textContent);
+// console.log(document.querySelector("#titulo-web"))
+// console.log(document.querySelectorAll(".text-danger"))
+// console.log(document.querySelectorAll(".container .text-danger"))
 
-const h1 = document.getElementById("titulo-web");
+// const h1 = document.getElementById("titulo-web");
 
-h1.textContent = "Nuevo texto desde js";
-h1.style.backgroundColor = "violet";
+// h1.textContent = "Nuevo texto desde js";
+// h1.style.backgroundColor = "violet";
+
+//Practica DOM
+const inputColor = document.querySelector('#inputColor');
+const btnVisualizar = document.querySelector('#btnVisualizar');
+const parrafoExa = document.querySelector('#parrafoExa');
+const cardColor = document.querySelector('#cardColor');
+
+//addEventListener
+console.log(inputColor.value);
+btnVisualizar.addEventListener("click",()=>{
+    console.log('Me diste click')
+    console.log(inputColor.value);
+    parrafoExa.textContent = inputColor.value;
+    cardColor.style.backgroundColor = inputColor.value;
+})
